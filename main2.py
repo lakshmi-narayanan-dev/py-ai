0caa1cf25e4f9ea1429bfb921d07e1fd42a43e1b
anisiva <anitha.sivasubramanian@gmail.com>
2024-04-09 16:56:04 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -3,6 +3,11 @@ from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
 import os
+import docx2txt
+import langchain.prompts as prompts
+from langchain.chains import LLMChain
+from langchain.chat_models import ChatOpenAI
+from pathlib import Path
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
@@ -18,9 +23,16 @@ def home():
     form = UploadFileForm()
     if form.validate_on_submit():
         file =  form.file.data #First Get the data
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) #save the file
-        return render_template('Success.html')
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename("CONTEXT.docx"))) #save the file
+        my_file = Path("/home/anisiva/py-ai/Static/Files/CONTEXT.docx")
+        if my_file.exists():
+            text = docx2txt.process("/home/anisiva/py-ai/Static/Files/CONTEXT.docx") 
+            print(text)
+        return "File Has Been Uploaded"
     return render_template('index.html', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+
