48bb40f436cd17c158bd41f136a9badb3175a62b
LAKSHMI NARAYANAN R <142524733+lakshmi-narayanan-dev@users.noreply.github.com>
2024-04-09 17:16:49 +0530

    Add files via upload

--- main2.py
+++ main2.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import FileField, SubmitField
+from wtforms import FileField, SubmitField, StringField 
 from werkzeug.utils import secure_filename
+from wtforms.validators import DataRequired
 import os
 import docx2txt
 import langchain.prompts as prompts
@@ -15,6 +16,11 @@ app.config['UPLOAD_FOLDER'] = 'Static/Files'
 
 class UploadFileForm(FlaskForm):
     file = FileField("File")
+
+ # Additional input fields
+    input1 = StringField("Input 1", validators=[DataRequired()])
+    # input2 = StringField("Input 2", validators=[DataRequired()])
+
     submit = SubmitField("Upload File")
 
 @app.route('/', methods=['GET', "POST"])
@@ -24,9 +30,9 @@ def home():
     if form.validate_on_submit():
         file =  form.file.data #First Get the data
         file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename("CONTEXT.docx"))) #save the file
-        my_file = Path("/home/anisiva/py-ai/Static/Files/CONTEXT.docx")
+        my_file = Path("/home/n6ine-dev/Downloads/Py-H-C/Static/Files/CONTEXT.docx")
         if my_file.exists():
-            text = docx2txt.process("/home/anisiva/py-ai/Static/Files/CONTEXT.docx") 
+            text = docx2txt.process("/home/n6ine-dev/Downloads/Py-H-C/Static/Files/CONTEXT.docx") 
             print(text)
         return "File Has Been Uploaded"
     return render_template('index.html', form=form)
@@ -34,5 +40,3 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
-
